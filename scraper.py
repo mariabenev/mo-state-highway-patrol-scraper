8e9ab89f63d1a225665799525543e1701cce625b
Maria Benevento <mfbxnx@umsystem.edu>
2020-12-11 16:29:11 -0600

    finished scraper

--- scraper.py
+++ scraper.py
@@ -43,12 +43,12 @@ def extract_incident_nums(search_results):
 
     tr_all = table.find_all('tr')[1:]
 
-    incident_nums = []
+    incident_nums = set()
 
     for tr in tr_all:
         url = tr.td.a.attrs['href']
         incident_num = url.split('ACC_RPT_NUM=')[1].strip()
-        incident_nums.append(incident_num)
+        incident_nums.add(incident_num)
 
     return incident_nums
 
@@ -65,22 +65,155 @@ def get_incident_report(incident_num):
 
     return r.content
 
+def extract_report_tables(incident_report):
+
+	soup = BeautifulSoup(incident_report, 'html5lib')
+
+	tables = soup.find_all('table')
+
+	return tables
+
+def extract_report_headers(table):
+
+	th_all = table.find_all('th')
+
+	headers = ['incident_number']
+
+	for th in th_all:
+		header = th.text.strip().replace(" ", "_").replace("/", '_').lower()
+		headers.append(header)
+
+	return headers
+
+def extract_report_rows(table, num):
+
+	tbody = table.find('tbody')
+
+	tr_all = tbody.find_all('tr')
+
+	rows = []
+
+	for tr in tr_all:
+		row = [num]
+		for td in tr.find_all('td'):
+			row.append(td.text.strip())
+		rows.append(row)
+
+	return rows
+
+def extract_table_data(table, num):
+	headers = extract_report_headers(table)
+	rows = extract_report_rows(table, num)
+	data = []
+	for row in rows:
+		dictionary = dict(zip(headers, row))
+		data.append(dictionary)
+	
+	return data
+
+def extract_crash_headers(table):
+	th_all = table.find_all('th')
+
+	headers = []
+
+	for th in th_all:
+		header = th.text.strip().replace(" ", "_").replace("/", '_').lower()
+		headers.append(header)
+	headers.append('misc_information')
+
+	return headers
+
+def extract_crash_rows (table, misc):
+	tbody = table.find('tbody')
+
+	tr_all = tbody.find_all('tr')
+
+	rows = []
+
+	for tr in tr_all:
+		row = []
+		for td in tr.find_all('td'):
+			row.append(td.text.strip())
+		row.append(misc)
+		rows.append(row)
+
+	return rows
+
+def extract_crash_data(table, misc):
+	headers = extract_crash_headers(table)
+	rows = extract_crash_rows(table, misc)
+	data = []
+	for row in rows:
+		dictionary = dict(zip(headers, row))
+		data.append(dictionary)
+	
+	return data
+
+def extract_misc_rows(table):
+
+	tbody = table.find('tbody')
+
+	tr = tbody.find('tr')
+
+	td = tr.find('td')
+
+	return td.text.strip()
 
 def main():
-    injury_types = get_injury_types()
+	all_crashes = []
+	all_vehicles = []
+	all_injuries = []
+	injury_types = get_injury_types()
 
-    for injury_type in injury_types[:1]:
-        search_results = get_search_results(injury_type)
-        incident_nums = extract_incident_nums(search_results)
-        
-        for num in incident_nums[:5]:
-            incident_report = get_incident_report(num)
+	for injury_type in injury_types:
+		search_results = get_search_results(injury_type)
+		incident_nums = extract_incident_nums(search_results)
 
-            print(incident_report)
-            print('---------------------------------')
+		for num in list(incident_nums):
+			incident_report = get_incident_report(num)
+			tables = extract_report_tables(incident_report)
+			vehicle_headers = extract_report_headers(tables[1])
+			injury_headers = extract_report_headers(tables[2])
+			misc = extract_misc_rows(tables[3])
+			crashes = extract_crash_data(tables[0], misc)
+			vehicles = extract_table_data(tables[1], num)
+			injuries = extract_table_data(tables[2], num)
+			all_crashes.append(crashes)
+			all_vehicles.append(vehicles)
+			all_injuries.append(injuries)
 
-            sleep(3)
+			sleep(3)
 
+	
+
+	with open('all_crashes.csv', 'w', newline='') as csvfile:
+		headers = ['investigated_by', 'incident#', 'gps_latitude', 'gps_longitude', 'date', 'time', 'county', 'location', 'troop', 'misc_information']
+		writer = csv.DictWriter(csvfile, fieldnames=headers)
+		writer.writeheader()
+
+		for rows in all_crashes:
+			for row in rows:
+				writer.writerow(row)
+
+	with open('all_vehicles.csv', 'w', newline='') as csvfile:
+		headers = vehicle_headers
+		writer = csv.DictWriter(csvfile, fieldnames=headers)
+		writer.writeheader()
+
+		for rows in all_vehicles:
+			for row in rows:
+				writer.writerow(row)
+
+	with open('all_injuries.csv', 'w', newline='') as csvfile:
+		headers = injury_headers
+		writer = csv.DictWriter(csvfile, fieldnames=headers)
+		writer.writeheader()
+
+		for rows in all_injuries:
+			for row in rows:
+				writer.writerow(row)
+
+	
 
 if __name__ == '__main__':
     main()
